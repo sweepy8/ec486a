212ffe4d6abb3699099978150da0a0dbdedd483c
Christian DeLozier <delozier@WRK1060057GOVT.academy.usna.edu>
2023-02-24 16:32:15 -0500

    Adding lab5 files

---
+++ lab5/Fixer.cpp
@@ -0,0 +1,84 @@
+#include "clang/Analysis/CFG.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+
+#include "llvm/Support/CommandLine.h"
+
+#include <cstring>
+#include <sstream>
+
+using namespace clang;
+using namespace clang::tooling;
+using namespace llvm;
+
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\nMore help text...\n");
+
+class FixerVisitor : public RecursiveASTVisitor<FixerVisitor> {
+  ASTContext *_context;
+  Rewriter *_rewriter;
+  
+public:
+  explicit FixerVisitor(ASTContext *context) : _context(context) {
+    LangOptions LO;
+    _rewriter = new Rewriter(_context->getSourceManager(),LO);
+  }
+
+  ~FixerVisitor(){
+    _rewriter->overwriteChangedFiles();
+    delete _rewriter;
+  }
+  
+  bool VisitVarDecl(VarDecl *varDecl){
+    // Only visit variables declared in the main file (not libraries)
+    if(!_context->getSourceManager().isInMainFile(varDecl->getBeginLoc())){
+      return true;
+    }
+
+    // TODO: add code here
+
+    return true;
+  }
+  
+};
+
+class FixerConsumer : public clang::ASTConsumer{
+  FixerVisitor _visitor;
+
+public:
+  explicit FixerConsumer(ASTContext *context) : _visitor(context) {}
+
+  virtual void HandleTranslationUnit(clang::ASTContext& Context) override {
+    _visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+};
+
+class Fixer : public clang::ASTFrontendAction{
+public:
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance& Compiler,
+								llvm::StringRef InFile) override {
+    return std::unique_ptr<clang::ASTConsumer>(new FixerConsumer(&Compiler.getASTContext()));
+  }
+};
+
+int main(int argc, const char **argv) {
+  llvm::Expected<CommonOptionsParser> OptionsParserE = CommonOptionsParser::create(argc, argv, MyToolCategory);
+  CommonOptionsParser& OptionsParser = OptionsParserE.get();
+  ClangTool Tool(OptionsParser.getCompilations(),
+		 OptionsParser.getSourcePathList());
+  return Tool.run(newFrontendActionFactory<Fixer>().get());
+}
---
+++ lecture_code/static_analysis/uninitialized4.cpp
@@ -0,0 +1,22 @@
+#include <cstring>
+
+class Point{
+public:
+  int x;
+  int y;
+};
+
+int main(){
+  int x;
+  int y;
+
+  char c;
+
+  int* p1;
+  Point* p2;
+
+  float array[32];
+  
+  y = x + 5;
+  return 0;
+}
