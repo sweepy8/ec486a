d101266cd2324bff3e3f73dd8ea1206c0921d4d3
Christian DeLozier <delozier@usna.edu>
2023-04-10 21:03:52 -0400

    Adding lab7

---
+++ lab7/tests/two.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+  int result;
+  int x = atoi(argv[1]);
+  int y = 6;
+  int z = 3;
+
+  x = x * 2;
+  z = z * y;
+  result = x + y + z;
+
+  printf("%d\n",result);
+
+  return 0;
+}
